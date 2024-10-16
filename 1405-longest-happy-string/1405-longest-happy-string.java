c6b2a9f0dadae22251267cfa89f42dcedffaf7d7
Ashwini Rasal <148693805+RasalAshwini@users.noreply.github.com>
2024-10-16 19:05:45 +0530

    Time: 2 ms (52.46%), Space: 41.2 MB (26.05%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -1,49 +1,52 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<Pair> pq= new PriorityQueue<>((x, y) -> y.num - x.num);
+        PriorityQueue<Pair> pq= new PriorityQueue<>((x,y)->y.num-x.num);
         if(a>0)
         {
             pq.add(new Pair(a,'a'));
         }
         if(b>0)
         {
-           pq.add(new Pair(b, 'b'));
+            pq.add(new Pair(b,'b'));
         }
         if(c>0)
         {
             pq.add(new Pair(c,'c'));
         }
-        StringBuilder result = new StringBuilder();
- while (!pq.isEmpty()) {
-            Pair curr = pq.poll(); 
-            int currCount = curr.num;
-            char currChar = curr.c;
-
-            
-            if (result.length() >= 2 && result.charAt(result.length() - 1) == currChar && result.charAt(result.length() - 2) == currChar) {
-                if (pq.isEmpty()) {
-                    break; 
+        StringBuilder result=new StringBuilder();
+        while( !pq.isEmpty())
+        {
+            Pair curr=pq.poll();
+            int currCount=curr.num;
+            char currChar=curr.c;
+            if(result.length() >= 2 && result.charAt(result.length()-1)==currChar && result.charAt(result.length()-2)==currChar)
+            {
+                if(pq.isEmpty())
+                {
+                    break;
                 }
-                Pair next = pq.poll(); 
-                result.append(next.c); 
-                next.num--; 
-                if (next.num > 0) {
-                    pq.add(next); 
+                Pair next=pq.poll();
+                result.append(next.c);
+                next.num--;
+                if(next.num >0)
+                {
+                    pq.add(next);
                 }
                 pq.add(curr);
-            } else {
-                result.append(currChar); 
-                currCount--; 
-                if (currCount > 0) {
-                    pq.add(new Pair(currCount, currChar)); 
+            }
+            else
+            {
+                result.append(currChar);
+                currCount--;
+                if(currCount >0 )
+                {
+                    pq.add(new Pair(currCount,currChar));
                 }
             }
         }
-
-        return result.toString(); 
+        return result.toString();
+        
     }
-
-    
     class Pair{
         int num;
         char c;
